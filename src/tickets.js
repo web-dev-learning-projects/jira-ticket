6078b07abdbaf9f3ed73fc99997646af9c379ef4
Yuyi-hao <roshanisingh2354@gmail.com>
2025-09-19 21:46:14 +0530

    [IMP] Remove note

--- src/tickets.js
+++ src/tickets.js
@@ -1,16 +1,20 @@
 import { uid } from "./utils.js";
 import { loadTickets, saveTickets } from "./storage.js";
 
+
 export function showTickets(){
     const noteCardsContainer = document.getElementById("note-card-container");
     noteCardsContainer.innerHTML = "";
-
+    
     let tickets = loadTickets();
     Object.values(tickets).forEach(({id, color, title, content}) => {
             const noteCard = document.createElement("div");
+            noteCard.setAttribute("id", id);
             noteCard.setAttribute("class", "note-card card");
             let noteCardInnerHtml = `
-        <button type="button" class="deleteBtn hidden"><i class="fa-solid fa-trash"></i><button>
+        <button type="button" class="deleteBtn hidden" id="noteDeleteBtn-${id}">
+            <i class="fa-solid fa-trash"></i>
+        <button>
         <div class="card-header">                
             <div class="card-color ${color}"></div>
             <h3 class="card-title">${title}</h3>
@@ -25,18 +29,27 @@ export function showTickets(){
                 <div class="lightyellow card-priority-color ${color=='lightyellow' ? 'selected': ''}"></div>
                 <div class="lightred    card-priority-color ${color=='lightred' ? 'selected': ''}"></div>
             </div>
-            <button class="btn" id="editBtn" onclick=removeNote><i class="fa-solid fa-lock"></i></button>
+            <button class="btn" id="editBtn-${id}"><i class="fa-solid fa-lock"></i></button>
         </div>`;
             noteCard.innerHTML = noteCardInnerHtml;
-            noteCard.querySelector("#editBtn").addEventListener("click", () => {editCard(noteCard);})
+            noteCard.querySelector(`#editBtn-${id}`).addEventListener("click", () => {editCard(noteCard, id);});
+            noteCard.querySelector(`#noteDeleteBtn-${id}`).addEventListener("click", () => {removeNote(tickets, id)});
             noteCardsContainer.appendChild(noteCard);
         }
     )
 }
 
-function editCard(card){
+function removeNote(tickets, id){
+    if(tickets[id]){
+        delete tickets[id];
+        saveTickets(tickets);
+    }
+    showTickets();
+}
+
+function editCard(card, id){
     // first
-    const editBtn = card.querySelector("#editBtn");
+    const editBtn = card.querySelector(`#editBtn-${id}`);
     if(editBtn.querySelector(".fa-lock")){
         editBtn.innerHTML = `<i class="fa-solid fa-lock-open"></i>`;
         card.querySelector(".noteContentText").readOnly = false;
