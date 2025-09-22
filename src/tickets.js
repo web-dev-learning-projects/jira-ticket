e1dd71ebf5d991ca6749d46d4a5e245ad6aae5a2
Yuyi-hao <roshanisingh2354@gmail.com>
2025-09-22 20:22:24 +0530

    [IMP] Implemented update ticket function

--- src/tickets.js
+++ src/tickets.js
@@ -11,17 +11,17 @@ export function showTickets(){
     Object.values(tickets).forEach(({id, color, title, content}) => {
             const noteCard = document.createElement("div");
             noteCard.setAttribute("id", id);
-            noteCard.setAttribute("class", "note-card card");
+            noteCard.setAttribute("class", "note-card card close");
             let noteCardInnerHtml = `
         <button type="button" class="deleteBtn hidden" id="noteDeleteBtn-${id}">
             <i class="fa-solid fa-trash"></i>
         <button>
         <div class="card-header">                
-            <div class="card-color ${color}"></div>
-            <h3 class="card-title">${title}</h3>
+            <div class="card-color ${color}" id="priority-${id}"></div>
+            <input class="card-title" type="text" readonly value="${title}" id="title-${id}"/>
         </div>
         <div class="card-body">
-            <textarea name="noteText" rows="10" class="noteContentText" readOnly>${content}</textarea>
+            <textarea name="noteText" rows="10" class="noteContentText" id="text-${id}" readOnly>${content}</textarea>
         </div>
         <div class="card-footer">
             <div class="priority-color-edit-container">
@@ -35,6 +35,20 @@ export function showTickets(){
             noteCard.innerHTML = noteCardInnerHtml;
             noteCard.querySelector(`#editBtn-${id}`).addEventListener("click", () => {editCard(noteCard, id);});
             noteCard.querySelector(`#noteDeleteBtn-${id}`).addEventListener("click", () => {removeNote(tickets, id)});
+            const priorityBtns = Array.from(noteCard.querySelectorAll(`.card-footer .priority-color-edit-container .card-priority-color`));
+            priorityBtns.forEach((btn) => {
+                btn.addEventListener("click", (e) => {
+                    if(noteCard.classList.contains("open")){
+                        priorityBtns.forEach((ele) => {
+                            ele.classList.remove('selected');
+                        });
+                        e.target.classList.add('selected');
+                        noteCard.querySelector(`#priority-${id}`).classList.remove(noteCard.querySelector(`#priority-${id}`).classList[1]);
+                        noteCard.querySelector(`#priority-${id}`).classList.add(e.target.classList[0]);
+                    }
+                });
+            });
+
             noteCardsContainer.appendChild(noteCard);
         }
     )
@@ -53,13 +67,24 @@ function editCard(card, id){
     const editBtn = card.querySelector(`#editBtn-${id}`);
     if(editBtn.querySelector(".fa-lock")){
         editBtn.innerHTML = `<i class="fa-solid fa-lock-open"></i>`;
+        card.classList.remove("close");
+        card.classList.add("open");
         card.querySelector(".noteContentText").readOnly = false;
+        card.querySelector(".card-title").readOnly = false;
         card.querySelector(".deleteBtn").classList.remove("hidden");
     }else{
-        console.log("unlocked")
+        card.classList.remove("open");
+        card.classList.add("close");
         editBtn.innerHTML = `<i class="fa-solid fa-lock"></i>`;
         card.querySelector(".noteContentText").readOnly = true;
+        card.querySelector(".card-title").readOnly = true;
         card.querySelector(".deleteBtn").classList.add("hidden");
+
+        // save the content of fields in
+        tickets[id].color =  card.querySelector(`#priority-${id}`).classList[1];
+        tickets[id].title =  card.querySelector(".card-title").value;
+        tickets[id].content = card.querySelector(".noteContentText").value;  
+        saveTickets(tickets);
     }
 }
 
